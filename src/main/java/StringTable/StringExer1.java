3144724cc7c02d067e08afc9dde948ca7285c8a1
zhou <2322965787@qq.com>
2025-04-23 17:13:37 +0800

    feat: String"+"操作和StringBuilder()在内存中的行为

--- src/main/java/StringTable/StringExer1.java
+++ src/main/java/StringTable/StringExer1.java
@@ -1,17 +1,18 @@
 package StringTable;
 
 public class StringExer1 {
-    public static void main(String[] args){
-        String x="ab";
-        String s=new String("a")+new String("b");//new String("ab)
-        String s2=s.intern();
-        //在上一行代码执行完以后，字符串常量池中并没有"ab"
-		/*
-		1、jdk6中：在字符串常量池（此时在永久代）中创建一个字符串"ab"
-        2、jdk8中：字符串常量池（此时在堆中）中没有创建字符串"ab",而是创建一个引用，指向new String("ab")，		  将此引用返回
-        3、详解看上面
-		*/
-        System.out.println(s2=="ab");
-        System.out.println(s=="ab");
+    public static void main(String[] args) {
+
+        String s = new String("1");//这行为会在常量池中创建一个1
+        s.intern();//调用此方法之前，字符串常量池中已经存在了"1"
+        String s2 = "1";
+        System.out.println(s == s2);//jdk6：false   jdk7/8：false
+        //"11"不存在常量池中
+        String s3 = new String("1") + new String("1");//
+        //s3.intern(),如果常量池中,那就不改变. 如果没有那就在常量池复制s3在堆区的引用
+        s3.intern();
+
+        String s4 = "11";//s4变量记录的地址：使用的是上一行代码代码执行时，在常量池中生成的"11"的地址
+        System.out.println(s3 == s4);//jdk6：false  jdk7/8：true
     }
 }
